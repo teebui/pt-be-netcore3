75276faf3035fb12a27ebd5b07cefc0e2ac0dbfa
Tuan-Anh BUI <tbui@TAB-MBP.local>
2020-05-10 22:48:43 +0200

    Create models for user and authentication implementation

---
+++ PropertyTrackApi/ViewModels/UserViewModel.cs
@@ -0,0 +1,27 @@
+using Models;
+
+namespace ViewModels
+{
+    public class UserViewModel
+    {
+        public UserViewModel()
+        {
+            // NOP
+        }
+        public UserViewModel(User user)
+        {
+            Id = user.Id;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+            Username = user.Username;
+            Token = user.Token;            
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Username { get; set; }
+        public string Token { get; set; }        
+    }
+    
+}
