fc2d99f97b73f58cba6c56b1da6cb8ca9e698901
Tuan-Anh BUI <tbui@TAB-MBP.local>
2020-06-27 17:06:12 +0200

    Just update

--- PropertyTrackApi/Controllers/CategoriesController.cs
+++ PropertyTrackApi/Controllers/CategoriesController.cs
@@ -60,7 +60,7 @@ namespace PropertyTrackApi.Controllers
         // To protect from overposting attacks, enable the specific properties you want to bind to, for
         // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutCategory(int id, Category category)
+        public async Task<IActionResult> UpdateCategory(int id, Category category)
         {
             if (id != category.Id)
             {
